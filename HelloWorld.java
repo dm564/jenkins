b2aadca8abe2e4dcf64696295a4a604a16c5c58a
dm564 <40348497+dm564@users.noreply.github.com>
2020-01-27 16:50:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,5 @@ public class HelloWorld
 	public static void main(String []srs)
 	{
 		System.out.println("Hello World!!!");
-		System.out.println("Hello Again!!");
-		System.out.println("Bye this time");
 	}
 }
